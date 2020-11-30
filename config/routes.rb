a7be4f402cbe2644b28a36db9889dcc04b07c128
Dasole Kwon <dasole1234@gmail.com>
2020-11-30 18:11:23 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+    put "/login", to: "login#login"
+    resources :users, only: [:index, :show, :create]
+    resources :courses, only: [:index, :show]
+    resources :answers, only: [:show, :create]
+    #   # /answers/:id/questions
+    #   resources :questions
+    #   # /answers/:id/choices
+    #   resources :choices
+    end
+  end
 end
+
