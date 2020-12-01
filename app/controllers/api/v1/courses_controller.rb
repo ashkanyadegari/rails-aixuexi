310d34c4fb4b6413fb78d6af758131de0c1bf1af
Wenqi Yu <kikiyu1006@gmail.com>
2020-12-01 18:00:14 +0800

    courses controller

---
+++ app/controllers/api/v1/courses_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::CoursesController < Api::V1::BaseController
+  before_action :set_course, only: [:show, :edit, :destroy]
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    render json: @course
+  end
+
+  def update
+    if @course.update(course_params)
+      render json: {course: @course, status: :updated}
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @course.destroy
+    render json: {status: :deleted}
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      render json: {status: :created}
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def render_error
+    render json: { errors: @course.errors.full_messages },
+      status: :unprocessable_entity
+  end
+end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  #index
+  #As a user I can see the progress of my teammates on the leaderboard.
+
+  #show
+  #As a user I can see my statistics.
+
+  #show[:id]
+  #As a user I can see my profile.
 
 end
