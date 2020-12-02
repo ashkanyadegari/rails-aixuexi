5ef830bcb06dc1d6424e1b63e544904f0f6c2131
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-12-02 14:51:20 +0800

    pushing on master

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   has_many :ongoing_courses
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many_attached :photos
   has_one_attached :file
 end
